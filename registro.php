a03f19b6b3f460aee9b36e19c28cad22f8009f33
Alan8-44 <61382366+Alan8-44@users.noreply.github.com>
2020-05-28 14:50:53 -0500

    Update registro.php

--- registro.php
+++ registro.php
@@ -5,9 +5,9 @@
 	$password_localhost ="Polizona-1";
 	$json=array();
 				
-		$opcion = $_GET["opciones"];
+		$opcion = $_GET["AL"];
 		$conexion = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-		$consulta="insert into empresas values (('".$opcion."');";
+		$consulta="insert into empresas values (('".$AL."');";
 		$resultado=mysqli_query($conexion,$consulta);
 		if($conexion){
 			echo "conectado: insert into empresas values (('".$opcion."');";
